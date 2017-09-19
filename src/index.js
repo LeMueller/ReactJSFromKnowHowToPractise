23bd12d7fd965e0df074c858513ab9eebb1146f3
lezhang <lezhang@techfak.uni-bielefeld.de>
2017-09-19 12:52:19 +0200

    clear class structure

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-import OnChange from './OnChange';
-import LikeButton from './LikeButton';
-import CartItem from './mixin/CartItem';
-import Stateful from './Stateful';
-import TimeUpdate from './practise1showtime/TimeUpdate';
-import DateUpdate from './practise1showtime/DateUpdate';
+import OnChange from './class03/OnChange.js';
+import LikeButton from './class04/LikeButton.js';
+import CartItem from './class04/mixin/CartItem.js';
+import Stateful from './class04/Stateful.js';
+
+import DateUpdate from './class04/practise1showtime/DateUpdate.js';
 
 ReactDOM.render(<DateUpdate />, document.getElementById('root'));
 registerServiceWorker();
